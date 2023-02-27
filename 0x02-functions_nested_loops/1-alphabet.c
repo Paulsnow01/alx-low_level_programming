ddf841c443e28e1fa90e1460d458024dacd22efa
Paul <akinleye.paul12gmail.com>
2023-02-27 00:13:57 -0800

    nestedloops

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,8 +1,8 @@
 #include "main.h"
 
 /**
- * main - print the alphabet in lowercase
- * folllowed by a new line
+ * print_alphabet - print the alphabet in lowercase
+ * followed by a new line
  * Return: always 0 if successful
  */
 void print_alphabet(void)
