49a764d5b417662cca3570c3024b1ea664532482
Paul <akinleye.paul12gmail.com>
2023-02-28 02:05:24 -0800

    :x! git # Please enter the commit message for your changes. Lines starting

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -3,8 +3,11 @@
 /**
  * jack_bauer - prints every minute of the day
  *
+ * Description: prints every minute of the day
+ *
  * Return: void
  */
+
 void jack_bauer(void)
 {
 	int hours_tens, hours_ones, minutes_tens, minutes_ones, hours_max;
@@ -34,14 +37,13 @@ void jack_bauer(void)
 					_putchar('\n');
 					minutes_ones++;
 				}
-				minutes_tens = '0';
-				hours_ones++;
+				minutes_ones = '0';
+				minutes_tens++;
 			}
-			hours_ones = '0';
+			minutes_tens = '0';
 			hours_ones++;
 		}
 		hours_ones = '0';
 		hours_tens++;
 	}
 }
-
