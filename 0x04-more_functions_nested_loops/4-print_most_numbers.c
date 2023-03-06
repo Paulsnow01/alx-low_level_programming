68035951d1ae0cd76e5b283d8d4ef5870da0b83a
Paul Akinleye <107161146+Paulsnow01@users.noreply.github.com>
2023-03-06 12:17:46 +0100

    Update 4-print_most_numbers.c

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -4,10 +4,8 @@
  * print_most_numbers - prints the numbers from 0 to 9
  * followed by a new line
  * do not print 2 and 4
- *
  * Return: always 0
  */
-
 void print_print_most_numbers(void)
 {
 	int i = '0';
