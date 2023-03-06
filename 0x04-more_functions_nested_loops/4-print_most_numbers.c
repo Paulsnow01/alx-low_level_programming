2f059b23ce102a6fe430857d1faf515dd50196f0
Paul <akinleye.paul12gmail.com>
2023-03-06 03:15:31 -0800

    nestin em loops

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -3,7 +3,6 @@
 /**
  * print_most_numbers - prints the numbers from 0 to 9
  * followed by a new line
- * @c: is an integer
  * do not print 2 and 4
  *
  * Return: always 0
@@ -11,11 +10,11 @@
 
 void print_print_most_numbers(void)
 {
-	int c = '0';
+	int i = '0';
 
-	for (; c <= '9'; c++)
+	for (; i <= '9'; i++)
 	{
-		if (c != '2' && c != '4')
+		if (i != '2' && i != '4')
 			_putchar(c);
 	}
 	_putchar('\n');
